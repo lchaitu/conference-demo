9b67446927558f98b86d1e652ec7037d2d1d1ef4
chaitanya <chaitanyakommareddy22@gmail.com>
2020-08-10 21:28:43 -0700

    comments`

--- src/main/java/com/chaitu/conferencedemo/ConferenceDemoApplication.java
+++ src/main/java/com/chaitu/conferencedemo/ConferenceDemoApplication.java
@@ -8,7 +8,7 @@ public class ConferenceDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConferenceDemoApplication.class, args);
-	//sample comment
+	//fytfvhjhj
     }
 
 }
